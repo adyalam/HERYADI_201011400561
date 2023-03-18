1724f0873cedee80951a7ff2857683fd97c5ba3b
adyalam <adyalam10@gmail.com>
2023-03-18 20:19:07 +0700

    UPDATE

--- app/src/main/java/com/ini_package_heryadi/myapplication/layout2.java
+++ app/src/main/java/com/ini_package_heryadi/myapplication/layout2.java
@@ -21,7 +21,7 @@ public class layout2 extends AppCompatActivity {
             public void onClick(View v) {
                 AlertDialog.Builder builder = new AlertDialog.Builder(layout2.this);
                 builder.setTitle("Keluar");
-                builder.setMessage("Anda mau tutup aplikasinya?");
+                builder.setMessage("Anda mau tutup aplikasinya ya?");
                 builder.setPositiveButton("Yoi", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
